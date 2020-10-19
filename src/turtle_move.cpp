865a96dfc00310cc1edde17b3525b5e37b43ecf4
mukmalone <michael.muldoon.home@gmail.com>
2020-10-18 23:26:23 -0400

    Integrated go to goal into turtle_move

--- src/turtle_move.cpp
+++ src/turtle_move.cpp
@@ -1,6 +1,16 @@
-#include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <turtlesim/Pose.h>
+#include <cmath>
 
+float x, y, theta;
+
+void poseCallback(const turtlesim::Pose::ConstPtr& msg)
+{
+    x = msg->x;
+    y = msg->y;
+    theta = msg->theta;
+}
 
 int main(int argc, char **argv)
 {
@@ -8,170 +18,31 @@ int main(int argc, char **argv)
  
     ros::NodeHandle n;
 
+    ros::Subscriber sub_pose = n.subscribe("turtle1/pose", 1000, poseCallback);
     ros::Publisher control_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 10);
  
-    ros::Rate loop_rate(.5);
+    ros::Rate loop_rate(10);
     int cnt = 0;
     geometry_msgs::Twist control_command;
+    control_command.linear.y = 0.0;
+    control_command.linear.z = 0.0;
+    control_command.angular.x = 0.0;
+    control_command.angular.y = 0.0;
+
+    //target
+    double target_x, target_y;
+    n.getParam("/object_origin_x", target_x);
+    n.getParam("/object_origin_y", target_y);
 
     while (ros::ok())
     {
-        if(cnt==0){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==1){
-            control_command.linear.x = 10.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==2){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 1.5707; 
-        }
-        else if(cnt==3){
-            control_command.linear.x = 0.5;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==4){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 1.5707; 
-        }
-        else if(cnt==5){
-            control_command.linear.x = 10.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==6){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = -1.5707; 
-        }
-        else if(cnt==7){
-            control_command.linear.x = 0.5;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==8){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = -1.5707; 
-        }
-        else if(cnt==9){
-            control_command.linear.x = 10.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==10){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 1.5707; 
-        }
-        else if(cnt==11){
-            control_command.linear.x = 0.5;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==12){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 1.5707; 
-        }
-        else if(cnt==13){
-            control_command.linear.x = 10.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==14){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = -1.5707; 
-        }
-        else if(cnt==15){
-            control_command.linear.x = 0.5;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==16){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = -1.5707; 
-        }
-        else if(cnt==17){
-            control_command.linear.x = 10.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
-        else if(cnt==18){
-            control_command.linear.x = 0.0;
-            control_command.linear.y = 0.0;
-            control_command.linear.z = 0.0;
-            control_command.angular.x = 0.0;
-            control_command.angular.y = 0.0;
-            control_command.angular.z = 0.0; 
-        }
+        control_command.linear.x = 1.5 * sqrt( pow(target_x-x,2) + pow(target_y-y,2) );            
+        control_command.angular.z = 4 * (atan2(target_y-y, target_x-x) - theta ); 
         control_pub.publish(control_command);
         ros::spinOnce();
         loop_rate.sleep();
         ++cnt;
+        ROS_INFO("Turtle is at: x [%f], y [%f], theta [%f]",x, y, theta);
     }
 
     return 0;
