e39d96943a42f25c9db6d70f6ece5be79bf25701
mukmalone <michael.muldoon.home@gmail.com>
2020-10-20 08:53:27 -0400

    Working on fixing angle velocity

--- src/turtle_move.cpp
+++ src/turtle_move.cpp
@@ -21,20 +21,20 @@ int main(int argc, char **argv)
     goals[0][0]=9.0; goals[0][1]=1.0;
     goals[1][0]=9.0; goals[1][1]=2.0;
     goals[2][0]=1.0; goals[2][1]=2.0;
-    goals[3][0]=9.0; goals[3][1]=3.0;
-    goals[4][0]=1.0; goals[4][1]=3.0;
+    goals[3][0]=1.0; goals[3][1]=3.0;
+    goals[4][0]=9.0; goals[4][1]=3.0;
     goals[5][0]=9.0; goals[5][1]=4.0;
     goals[6][0]=1.0; goals[6][1]=4.0;
-    goals[7][0]=9.0; goals[7][1]=5.0;
-    goals[8][0]=1.0; goals[8][1]=5.0;
+    goals[7][0]=1.0; goals[7][1]=5.0;
+    goals[8][0]=9.0; goals[8][1]=5.0;
     goals[9][0]=9.0; goals[9][1]=6.0;
     goals[10][0]=1.0; goals[10][1]=6.0;
-    goals[11][0]=9.0; goals[11][1]=7.0;
-    goals[12][0]=1.0; goals[12][1]=7.0;
+    goals[11][0]=1.0; goals[11][1]=7.0;
+    goals[12][0]=9.0; goals[12][1]=7.0;
     goals[13][0]=9.0; goals[13][1]=8.0;
     goals[14][0]=1.0; goals[14][1]=8.0;
-    goals[15][0]=9.0; goals[15][1]=9.0;
-    goals[16][0]=1.0; goals[16][1]=9.0;
+    goals[15][0]=1.0; goals[15][1]=9.0;
+    goals[16][0]=9.0; goals[16][1]=9.0;
     goals[17][0]=9.0; goals[17][1]=10.0;
     goals[18][0]=1.0; goals[18][1]=10.0;
 
@@ -45,8 +45,9 @@ int main(int argc, char **argv)
     ros::Subscriber sub_pose = n.subscribe("turtle1/pose", 1000, poseCallback);
     ros::Publisher control_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 10);
  
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(20);
     int cnt = 0;
+    const double pi = 3.14159265358979323846;
     geometry_msgs::Twist control_command;
     control_command.linear.y = 0.0;
     control_command.linear.z = 0.0;
@@ -58,15 +59,20 @@ int main(int argc, char **argv)
     while (ros::ok() && !complete)
     {
         control_command.linear.x = 1.5 * sqrt( pow(goals[target_num][0]-x,2) + pow(goals[target_num][1]-y,2) );            
-        control_command.angular.z = 3 * (atan2(goals[target_num][1]-y, goals[target_num][0]-x) - theta ); 
+        float angle = atan2(goals[target_num][1]-y, goals[target_num][0]-x)- theta;
+        //working on angles
+        if (angle<=-3.14 || angle>3.14){
+            angle=angle/pi;
+        }
+        control_command.angular.z = 10 * (angle); 
         control_pub.publish(control_command);
         ros::spinOnce();
         loop_rate.sleep();
         ++cnt;
-        ROS_INFO("Turtle is at: x [%f], y [%f], theta [%f]",x, y, theta);
+        ROS_INFO("Turtle cmd: x [%f], ang.z [%f]",control_command.linear.x, control_command.angular.z);
 
         //check if we are at the goal within tolerance
-        double dx, dy;
+        double dx, dy, tolerance=0.1;
         dx = abs(x-goals[target_num][0]);
         dy = abs(y-goals[target_num][1]);
 
