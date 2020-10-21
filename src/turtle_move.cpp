f1ee1bc66dfad25a18ee81060b9e95b1c16acad2
mukmalone <michael.muldoon.home@gmail.com>
2020-10-20 20:14:26 -0400

    Solved angle issue by ensuring angle>-pi and <pi

--- src/turtle_move.cpp
+++ src/turtle_move.cpp
@@ -17,26 +17,26 @@ int main(int argc, char **argv)
 {
     //fixed zig-zag search pattern     
     int num_goals=19;
-    double goals[num_goals][2];
-    goals[0][0]=9.0; goals[0][1]=1.0;
-    goals[1][0]=9.0; goals[1][1]=2.0;
-    goals[2][0]=1.0; goals[2][1]=2.0;
-    goals[3][0]=1.0; goals[3][1]=3.0;
-    goals[4][0]=9.0; goals[4][1]=3.0;
-    goals[5][0]=9.0; goals[5][1]=4.0;
+    float goals[num_goals][2];
+    goals[0][0]=10.0; goals[0][1]=1.0;
+    goals[1][0]=10.0; goals[1][1]=10.0;
+    goals[2][0]=1.0; goals[2][1]=10.0;
+    goals[3][0]=1.0; goals[3][1]=1.0;
+    goals[4][0]=10.0; goals[4][1]=3.0;
+    goals[5][0]=10.0; goals[5][1]=4.0;
     goals[6][0]=1.0; goals[6][1]=4.0;
     goals[7][0]=1.0; goals[7][1]=5.0;
-    goals[8][0]=9.0; goals[8][1]=5.0;
-    goals[9][0]=9.0; goals[9][1]=6.0;
+    goals[8][0]=10.0; goals[8][1]=5.0;
+    goals[9][0]=10.0; goals[9][1]=6.0;
     goals[10][0]=1.0; goals[10][1]=6.0;
     goals[11][0]=1.0; goals[11][1]=7.0;
-    goals[12][0]=9.0; goals[12][1]=7.0;
-    goals[13][0]=9.0; goals[13][1]=8.0;
+    goals[12][0]=10.0; goals[12][1]=7.0;
+    goals[13][0]=10.0; goals[13][1]=8.0;
     goals[14][0]=1.0; goals[14][1]=8.0;
     goals[15][0]=1.0; goals[15][1]=9.0;
-    goals[16][0]=9.0; goals[16][1]=9.0;
-    goals[17][0]=9.0; goals[17][1]=10.0;
-    goals[18][0]=1.0; goals[18][1]=10.0;
+    goals[16][0]=10.0; goals[16][1]=9.0;
+    goals[17][0]=10.0; goals[17][1]=10.0;
+    goals[18][0]=1.0; goals[18][1]=9.0;
 
     ros::init(argc, argv, "turtle_move");
  
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
  
     ros::Rate loop_rate(20);
     int cnt = 0;
-    const double pi = 3.14159265358979323846;
+    const float pi = 3.14159265358979323846;
     geometry_msgs::Twist control_command;
     control_command.linear.y = 0.0;
     control_command.linear.z = 0.0;
@@ -59,10 +59,13 @@ int main(int argc, char **argv)
     while (ros::ok() && !complete)
     {
         control_command.linear.x = 1.5 * sqrt( pow(goals[target_num][0]-x,2) + pow(goals[target_num][1]-y,2) );            
-        float angle = atan2(goals[target_num][1]-y, goals[target_num][0]-x)- theta;
+        float angle = atan2(goals[target_num][1]-y, goals[target_num][0]-x) - theta;
         //working on angles
-        if (angle<=-3.14 || angle>3.14){
-            angle=angle/pi;
+        if (angle<-pi){
+            angle+=2*pi;
+        }
+        else if (angle>pi){
+            angle-=2*pi;
         }
         control_command.angular.z = 10 * (angle); 
         control_pub.publish(control_command);
@@ -72,7 +75,7 @@ int main(int argc, char **argv)
         ROS_INFO("Turtle cmd: x [%f], ang.z [%f]",control_command.linear.x, control_command.angular.z);
 
         //check if we are at the goal within tolerance
-        double dx, dy, tolerance=0.1;
+        float dx, dy, tolerance=0.1;
         dx = abs(x-goals[target_num][0]);
         dy = abs(y-goals[target_num][1]);
 
